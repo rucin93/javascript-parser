56d2cc4ff885a5b13a59d110e47ce23cc2c25ad6
rucin93 <rucin93@gmail.com>
2020-09-25 01:31:33 +0200

    new parsers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,43 @@
-import { createCharDec } from './utils'
+import {createCharDec} from './utils'
+
+function enhancedTextParser(code: string): string {
+    code = ';' + code.replace(/(\r\n|\n|\r)/gm, '\\n');
+
+    const s = code
+    let V = '';
+    const W = [...new Set([...(';' + s)])].join('')
+
+    let S = 0;
+    let q = '';
+    for (let i = 0; (q = s[i]) || i % 4; ++i){
+        S += q ? W.indexOf(q) << i % 2 * 5 : 0
+        i % 2 ? (V += String.fromCharCode(((i % 4 == 1 ? 54 : 55) << 10) + S), S = 0) : 0
+    }
+// build the output string
+//     `for(I=O="";I<1e5;)O+=\`;print(${W})\`["${V}".charCodeAt(I/2)>>I++%2*5&31]
+// eval(O)`
+    return `for(I=O="";I<1e5;)O+=\`${W}\`["${V}".charCodeAt(I/2)>>I++%2*5&31];eval(\`print('\${O})')\`)`
+}
+
+function anotherTextParser(code:string) :string {
+    let u = ''
+    const m = 95
+    const r = 32
+    const mod = (n,m) => (n%m+m)%m
+    code = ';' + code.replace(/(\r\n|\n|\r)/gm, '\\n');
+    code += '//';
+    code += ' '.repeat(code.length*2%3)
+    const step = code.length / 3
+    for(let i=0; i<step; i++){
+        const [a, b, c] = [code[i+2*step], code[i+step], code[i]].map(c=>c.charCodeAt(0)-r)
+        const x = mod(a-b, m+1)*m + a
+        let y = mod((m+1)/2*(x-c), m+2)*m*(m+1) + x
+        if(y >= 0xD800 && y <= 0xDFFF) y+=m*(m+1)*(m+2)
+        u += String.fromCodePoint(y)
+    }
+
+    return `for(_=i=98;i--;)for(c of\`${u}\`)_+=String.fromCharCode(c.codePointAt()%i+32);eval(_)`
+}
 
 function textParser(code: string): string {
     let _ = ';' + code.replace(/(\r\n|\n|\r)/gm, '\\n');
@@ -23,9 +62,9 @@ function numbersParser(code: string): string {
     const rest = length % spreadSize;
     rest !== 0 ? _ += '0'.repeat(spreadSize - rest) : '';
 
-    const test = _.match(new RegExp(`.{${spreadSize}}`,'g')).map(num => createCharDec(num)).join(``)
+    const test = _.match(new RegExp(`.{${spreadSize}}`, 'g')).map(num => createCharDec(num)).join(``)
     let result = `m='';for(x of\`${test}\`)m+=\`\${x.codePointAt()}\`.padStart(${spreadSize},0)`
-    return rest !== 0 ? result += `;m=m.slice(0,-${spreadSize - rest})` : result
+    return rest !== 0 ? result + `;m=m.slice(0,-${spreadSize - rest})` : result
 }
 
 export default function parseCode(code: string, type: string): string {
@@ -33,7 +72,9 @@ export default function parseCode(code: string, type: string): string {
     if (type.toUpperCase() === 'NUMBERS') {
         result = numbersParser(code);
     } else {
-        result = textParser(code);
+        // result = enhancedTextParser(code);
+        result = anotherTextParser(code);
+        // result = textParser(code);
     }
 
     return result;
